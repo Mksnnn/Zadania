3da6cec4de5477ae3918b998b9c24982addef6e0
Mksnnn <95303877+Mksnnn@users.noreply.github.com>
2025-07-01 23:41:12 +0200

    Zad3

---
+++ Zadanie 3/Zadanie 3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+string path = "C:/test/users-" + DateTime.Now.ToString("HH_mm_dd_MM_yyyy")+".txt";
+
+string[] imiona = {"Ania", "Kasia", "Basia", "Zosia"};
+string[] nazwiska = {"Kowalska", "Nowak"};
+
+Random r = new Random();
+
+File.WriteAllText(path, "LP,Imie,Nazwisko,Rok urodzenia\n");
+for(int i=0;i<100;i++)
+{
+    string s = i.ToString() + ",";
+    s += imiona[r.Next(imiona.Length)] + ",";
+    s += nazwiska[r.Next(nazwiska.Length)] + ",";
+    s += r.Next(1990, 2000).ToString() + "\n";
+    File.AppendAllText(path, s);
+}
