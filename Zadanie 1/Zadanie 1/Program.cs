e5726a194922756f651b60061db147c326f0ef33
Mksnnn <95303877+Mksnnn@users.noreply.github.com>
2025-07-01 23:40:20 +0200

    Zad1

---
+++ Zadanie 1/Zadanie 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+string path = "C:/test/test_inf_tec.txt";
+
+string s;
+int n = 0;
+
+try 
+{
+    s = File.ReadAllText(path);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.ToString());
+    return;
+}
+
+for(int i=0;i<s.Length;i++)
+{
+    if (s[i] == 'a')
+    {
+        n++;
+    }
+}
+Console.WriteLine("Ilość \"a\" w tekście: " + n);
