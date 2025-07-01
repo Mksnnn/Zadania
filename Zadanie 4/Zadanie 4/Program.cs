b2adf3d2a47fccc702314db0019352c6df4e33ad
Mksnnn <95303877+Mksnnn@users.noreply.github.com>
2025-07-01 23:41:50 +0200

    Zad4

---
+++ Zadanie 4/Zadanie 4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+Console.Write("Podaj PLN: ");
+
+double pln = double.Parse(Console.ReadLine());
+
+var client = new HttpClient();
+var response = await client.GetStringAsync("https://api.nbp.pl/api/exchangerates/rates/a/usd");
+
+using JsonDocument jdoc = JsonDocument.Parse(response);
+JsonElement root = jdoc.RootElement;
+
+double kurs = root.GetProperty("rates")[0].GetProperty("mid").GetDouble();
+Console.Write(Math.Round(pln,2).ToString()+"zł = $"+Math.Round((pln/kurs),2).ToString());
