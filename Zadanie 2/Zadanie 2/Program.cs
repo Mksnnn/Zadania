546ae92c6d87e7e30b588ae242a7c46156c03959
Mksnnn <95303877+Mksnnn@users.noreply.github.com>
2025-07-01 23:40:47 +0200

    Zad2

---
+++ Zadanie 2/Zadanie 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+string path;
+string newPath;
+string s;
+
+path = Console.ReadLine();
+path.Replace('\\', '/');
+
+if(File.Exists(path))
+{
+    s = File.ReadAllText(path);
+    s = s.Replace("praca", "job");
+    newPath = path.Substring(0, path.Length - 4);
+    newPath += "_changed-" + DateTime.Today.ToString("dd MM yyyy") + ".txt";
+    File.WriteAllText(newPath, s);
+}else
+{
+    Console.WriteLine("Plik nie istnieje, spróbuj ponownie.");
+    path = Console.ReadLine();
+    path.Replace('\\', '/');
+}
+    
+
